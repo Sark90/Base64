1756f52a46647a4c79a9f957a1312098019a085a
Sark90 <mmg9658@gmail.com>
2018-03-23 11:57:21 +0200

    comment deleted

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
-    public static void main(String[] args) /*throws UnsupportedEncodingException*/ {
+    public static void main(String[] args) {
         Decoder.decode(Encoder.encode("zoo.jpg"), "out.jpg");
     }
 }
